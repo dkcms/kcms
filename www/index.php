4695616594b79ec2f7c37193f359ee233c186f4d
dkcms <58633160+dkcms@users.noreply.github.com>
2019-12-11 20:39:03 +0800

    Update index.php

--- www/index.php
+++ www/index.php
@@ -106,7 +106,7 @@ if(!empty($_GET['id'])&&!empty($_GET['i'])){
 }
 if(!empty($_GET['id'])&&!empty($_GET['g'])){
     preg_match('/^[0-9]{1,}$/', $_GET['id'], $id);
-    echo '是否被墙第 '.$id[0].' 条数据检验';
+    echo '检测网站第 '.$id[0].' 条数据是否被墙';
     if(!empty($dom[$id[0]])){
         $ch = curl_init();
         $options =  array(
