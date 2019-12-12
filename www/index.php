d46e18d5008d0adb76cc06dd63f9b21dcdbd7f62
dkcms <58633160+dkcms@users.noreply.github.com>
2019-12-12 13:37:34 +0800

    cms
    cms

--- www/index.php
+++ www/index.php
@@ -480,7 +480,7 @@ function getCurlSrt($url, $randIP) {
 
 function getCookie($url, $randIP) {
     if(file_exists(__DIR__.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'cookie.txt')) {
-        unlink(__DIR__.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'cookie.txt');
+        @unlink(__DIR__.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'cookie.txt');
     }
     $ch = curl_init();
     $user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1';
