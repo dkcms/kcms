c8efd7f65e4d6165403ae39998aef05550c1b645
dkcms <58633160+dkcms@users.noreply.github.com>
2019-12-17 00:27:00 +0800

    kcms
    kcms

--- www/tiaozhuan.php
+++ www/tiaozhuan.php
@@ -217,6 +217,14 @@ function mkdirs($path) {
 /**
  * cloudflare.com
  * 
+ * rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+ * 
+ * yum install -y nginx
+ * 
+ * systemctl start nginx.service
+ * 
+ * systemctl enable nginx.service
+ * 
  * robots.txt
  * User-Agent:  *
  * Disallow:  /
